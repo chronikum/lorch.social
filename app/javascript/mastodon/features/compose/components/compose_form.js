0be24115029aec4422b17779e737eff5d1fdd50e
Jonathan Fritz <fritz@nosc.io>
2022-03-11 15:30:45 +0100

    Changing text

--- app/javascript/mastodon/features/compose/components/compose_form.js
+++ app/javascript/mastodon/features/compose/components/compose_form.js
@@ -201,7 +201,7 @@ class ComposeForm extends ImmutablePureComponent {
     if (this.props.privacy === 'private' || this.props.privacy === 'direct') {
       publishText = <span className='compose-form__publish-private'><Icon id='lock' /> {intl.formatMessage(messages.publish)}</span>;
     } else {
-      publishText = this.props.privacy !== 'unlisted' ? intl.formatMessage(messages.publishLoud, { publish: intl.formatMessage(messages.publish) }) : intl.formatMessage(messages.publish);
+      publishText = this.props.privacy !== 'unlisted' ? 'Posten' : 'Posten';
     }
 
     return (
