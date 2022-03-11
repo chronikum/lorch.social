83f850df1834be6ac4721cf904e2f319ecce2afd
Jonathan Fritz <fritz@nosc.io>
2022-03-12 00:00:45 +0100

    Add search bar

--- app/javascript/mastodon/features/compose/components/search_results.js
+++ app/javascript/mastodon/features/compose/components/search_results.js
@@ -9,7 +9,7 @@ import Hashtag from '../../../components/hashtag';
 import Icon from 'mastodon/components/icon';
 import { searchEnabled } from '../../../initial_state';
 import LoadMore from 'mastodon/components/load_more';
-import SearchContainerPrefix from 'mastodon/features/compose/containers/search_container_prefix';
+import Search from '../components/search';
 
 const messages = defineMessages({
   dismissSuggestion: { id: 'suggestions.dismiss', defaultMessage: 'Dismiss suggestion' },
@@ -130,6 +130,8 @@ class SearchResults extends ImmutablePureComponent {
           <Icon id='search' fixedWidth />
           <FormattedMessage id='search_results.total' defaultMessage='{count, number} {count, plural, one {result} other {results}}' values={{ count }} />
         </div>
+        <p>Test Nachricht</p>
+        <Search />
         <p>Test Nachricht</p>
 
         {accounts}
