82e770ca9a206f35d4de1389b0d0016e2c83c456
Jonathan Fritz <fritz@nosc.io>
2022-03-13 15:14:18 +0100

    Adding info text and adding red logout button

--- app/javascript/mastodon/features/ui/components/link_footer.js
+++ app/javascript/mastodon/features/ui/components/link_footer.js
@@ -48,21 +48,19 @@ class LinkFooter extends React.PureComponent {
     return (
       <div className='getting-started__footer'>
         <ul>
-          {invitesEnabled && <li><a href='/invites' target='_blank'><FormattedMessage id='getting_started.invite' defaultMessage='Invite people' /></a> · </li>}
-          {withHotkeys && <li><Link to='/keyboard-shortcuts'><FormattedMessage id='navigation_bar.keyboard_shortcuts' defaultMessage='Hotkeys' /></Link> · </li>}
-          <li><a href='/auth/edit'><FormattedMessage id='getting_started.security' defaultMessage='Security' /></a> · </li>
-          <li><a href='/about/more' target='_blank'><FormattedMessage id='navigation_bar.info' defaultMessage='About this server' /></a> · </li>
-          <li><a href='https://joinmastodon.org/apps' target='_blank'><FormattedMessage id='navigation_bar.apps' defaultMessage='Mobile apps' /></a> · </li>
-          <li><a href='/terms' target='_blank'><FormattedMessage id='getting_started.terms' defaultMessage='Terms of service' /></a> · </li>
-          <li><a href='/settings/applications' target='_blank'><FormattedMessage id='getting_started.developers' defaultMessage='Developers' /></a> · </li>
-          <li><a href='https://docs.joinmastodon.org' target='_blank'><FormattedMessage id='getting_started.documentation' defaultMessage='Documentation' /></a> · </li>
-          <li><a href='/auth/sign_out' onClick={this.handleLogoutClick}><FormattedMessage id='navigation_bar.logout' defaultMessage='Logout' /></a></li>
+          {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
+          <span className='button-danger' href='/auth/sign_out' onClick={this.handleLogoutClick}>Logout</span>
         </ul>
 
         <p>
           <FormattedMessage
-            id='getting_started.open_source_notice'
-            defaultMessage='Mastodon is open source software. You can contribute or report issues on GitHub at {github}.'
+            id='getting_started.open_source_notice_updated'
+            defaultMessage='Dieses Projekt ist ein Fork von dem quelloffenem Mastodon und wird ehrenamtlich betreut. Technische Fehler können Sie @admin melden.'
+            values={{ github: <span><a href={source_url} rel='noopener noreferrer' target='_blank'>{repository}</a> (v{version})</span> }}
+          />
+          <FormattedMessage
+            id='getting_started.privacy'
+            defaultMessage='Diese Seite gibt keine persönlichen Informationen an Dritte weiter, die Daten werden weder analysiert noch gesammelt. Sie läuft auf einem sicheren Server in Deutschland.'
             values={{ github: <span><a href={source_url} rel='noopener noreferrer' target='_blank'>{repository}</a> (v{version})</span> }}
           />
         </p>
