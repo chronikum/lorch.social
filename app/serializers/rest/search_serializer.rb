3bfc6e636b4cfb30b094eb2dc680465490031cc1
Jonathan Fritz <fritz@nosc.io>
2022-03-14 10:42:06 +0100

    Quick testing

--- app/serializers/rest/search_serializer.rb
+++ app/serializers/rest/search_serializer.rb
@@ -3,6 +3,6 @@
 class REST::SearchSerializer < ActiveModel::Serializer
   has_many :accounts, serializer: REST::AccountSerializer
   has_many :statuses, serializer: REST::StatusSerializer
-  has_many :more, serializer: REST::StatusSerializer
+  has_many :more
   has_many :hashtags, serializer: REST::TagSerializer
 end
