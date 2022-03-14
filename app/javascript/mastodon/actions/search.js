62fb0c3a015b52d8feb953959c7d5dec493befeb
Jonathan Fritz <fritz@nosc.io>
2022-03-14 10:14:40 +0100

    Debugging frontend for new values

--- app/javascript/mastodon/actions/search.js
+++ app/javascript/mastodon/actions/search.js
@@ -62,7 +62,15 @@ export function submitSearch(tags) {
       }
 
       if (response.data.statuses) {
+		console.log('ORIGINAL DATA')
+        console.log(response.data.statuses);
         dispatch(importFetchedStatuses(response.data.statuses));
+      }
+	  
+	  if (response.data.more) {
+        console.log('MORE DATA')
+        console.log(response.data.more);
+        // dispatch(importFetchedStatuses(response.data.more));
       }
 
       dispatch(fetchSearchSuccess(response.data, value));
